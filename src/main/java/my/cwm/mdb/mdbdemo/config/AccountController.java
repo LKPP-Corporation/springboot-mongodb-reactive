6f443cc1149b2dd686ae0a64c45b066a14721227
Che Wan Mahadzir <cwmahadzir@gmail.com>
2020-09-29 23:39:55 +0800

    setel login

--- src/main/java/my/cwm/mdb/mdbdemo/config/AccountController.java
+++ src/main/java/my/cwm/mdb/mdbdemo/config/AccountController.java
@@ -1,12 +1,15 @@
 package my.cwm.mdb.mdbdemo.config;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import lombok.extern.slf4j.Slf4j;
 import my.cwm.mdb.mdbdemo.features.security.AccountService;
+import my.cwm.mdb.mdbdemo.features.security.TokenPair;
 import my.cwm.mdb.mdbdemo.features.security.UserLogin;
 import reactor.core.publisher.Mono;
 import org.springframework.web.reactive.function.BodyInserters;
@@ -14,6 +17,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 import javax.validation.Valid;
 
+@Slf4j
 @RestController
 public class AccountController {
 
@@ -31,11 +35,14 @@ public class AccountController {
     }
 
     @PostMapping(PATH_POST_LOGIN)
-    public Mono<ServerResponse> login(@Valid @RequestBody UserLogin login) {
+    public Mono<TokenPair> login(@Valid @RequestBody UserLogin login) {
         // Mono<UserLogin> loginRequest = request.bodyToMono(UserLogin.class);
         // return loginRequest.flatMap(lr -> {
         return this.accountService.login(login).flatMap(tp -> {
-                return ServerResponse.ok().contentType(APPLICATION_JSON).body(BodyInserters.fromValue(tp));
+            // log.debug(tp.toString());
+            return Mono.just(tp);
+            // return
+            // ServerResponse.ok().contentType(APPLICATION_JSON).body(BodyInserters.fromValue(tp));
             });
             // .onErrorContinue(ServerResponse.badRequest()
             // .body(BodyInserters.fromObject(new ApiResponse(400, "Invalid credentials",
--- src/main/java/my/cwm/mdb/mdbdemo/features/security/TokenPair.java
+++ src/main/java/my/cwm/mdb/mdbdemo/features/security/TokenPair.java
@@ -4,9 +4,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 @AllArgsConstructor
 @Getter
+@ToString
 public class TokenPair {
     private final String jwt;
 
